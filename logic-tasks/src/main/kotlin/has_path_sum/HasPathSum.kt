899cfa09bbed0d04dde6d65994b6e58dea4e7072
Dmitriy Fialkovskiy <dmitriy.fialkovskiy@gmail.com>
2022-10-08 05:33:02 +0300

    HasPathSum upd - slower runtime when removing duplication code

--- logic-tasks/src/main/kotlin/has_path_sum/HasPathSum.kt
+++ logic-tasks/src/main/kotlin/has_path_sum/HasPathSum.kt
@@ -26,19 +26,37 @@ fun main() {
 class HasPathSum {
 
     companion object {
+        // Runtime: 187 ms, faster than 98.90% of Kotlin online submissions for Path Sum.
+        // Memory Usage: 35.9 MB, less than 89.43% of Kotlin online submissions for Path Sum.
         fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
             if (root?.`val` == targetSum && root.left == null && root.right == null) return true
             return (if (root?.left != null) {
-                root.left?.`val` = root.left?.`val`?.plus(root.`val`)!!
+                root.left?.apply { `val` += root.`val` }
                 if (root.left?.`val` == targetSum && root.left == null && root.right == null) return true
                 hasPathSum(root.left, targetSum)
             } else false)
                 .or(if (root?.right != null) {
-                    root.right?.`val` = root.right?.`val`?.plus(root.`val`)!!
+                    root.right?.apply { `val` += root.`val` }
                     if (root.right?.`val` == targetSum && root.left == null && root.right == null) return true
                     hasPathSum(root.right, targetSum)
+                } else false)
+        }
+
+        // Runtime: 344 ms, faster than 31.28% of Kotlin online submissions for Path Sum.
+        // Memory Usage: 38.5 MB, less than 35.68% of Kotlin online submissions for Path Sum.
+        fun hasPathSumSlower(root: TreeNode?, targetSum: Int): Boolean {
+            if (root?.`val` == targetSum && root.left == null && root.right == null) return true
+            return depthCheck(root?.left, targetSum, root?.`val`).or(depthCheck(root?.right, targetSum, root?.`val`))
+        }
+
+        private fun depthCheck(node: TreeNode?, targetSum: Int, rootVal: Int?): Boolean {
+            return (if (node != null) {
+                node.apply { `val` += rootVal!! }
+                if (node.`val` == targetSum && node.left == null && node.right == null) return true
+                hasPathSum(node, targetSum)
             } else false)
         }
+
     }
 
     //Definition for a binary tree node.
