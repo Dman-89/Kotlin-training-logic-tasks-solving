a29700bd84ed1240225933a133c47df72dbd6d46
Konstantin Barabashkov <kbarabashkov@yandex.ru>
2022-10-10 14:48:49 +0400

    fix inject number

--- spring-training/src/main/kotlin/annotations_processing/InjectNumberAnnotationBeanPostProcessor.kt
+++ spring-training/src/main/kotlin/annotations_processing/InjectNumberAnnotationBeanPostProcessor.kt
@@ -2,22 +2,23 @@ package annotations_processing
 
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
-import org.springframework.util.ReflectionUtils
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.jvm.isAccessible
 
 @Component
-class InjectNumberAnnotationBeanPostProcessor: BeanPostProcessor {
-
-    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
-//        return super.postProcessBeforeInitialization(bean, beanName)
-        if (bean.javaClass == CounterImpl::class.java) {
-            val fields = bean.javaClass.declaredFields
-            fields.forEach {
-                val injectNumber = it.getAnnotation(InjectNumber::class.java)
-//                if (injectNumber != null) {
-                    println("${bean.javaClass} has declaredFields: $fields")
-//                    println("injecting number ${injectNumber.num}")
-                    if (it.declaringClass == Number::class.java && it.trySetAccessible()) ReflectionUtils.setField(it, bean, injectNumber.num)
-//                }
+class InjectNumberAnnotationBeanPostProcessor : BeanPostProcessor {
+    //https://stackoverflow.com/questions/57582135/kotlin-annotation-not-in-fielddecleration-or-compiled-java
+    //https://stackoverflow.com/questions/28391889/kotlin-reflection-get-list-of-fields
+    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+        val fields = bean::class.declaredMemberProperties
+        fields.filterIsInstance<KMutableProperty<*>>().forEach { field ->
+            val injectNumber = field.findAnnotation<InjectNumber>()
+            println("${bean.javaClass} has declaredFields: $fields")
+            if (injectNumber != null) {
+                field.isAccessible = true
+                field.setter.call(bean, injectNumber.num)
             }
         }
         return bean
