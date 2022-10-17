8acf48f7544a745fe4ac8e7a0c1a6077c8e00c36
Dmitriy Fialkovskiy <d.g.fialka@gmail.com>
2022-10-17 22:29:04 +0300

    spring BeanPostProcessor training

--- spring-training/src/main/kotlin/annotations_processing/CounterImpl.kt
+++ spring-training/src/main/kotlin/annotations_processing/CounterImpl.kt
@@ -2,20 +2,29 @@ package annotations_processing
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 class CounterImpl(@Value("\${counter.start}") var start: Int): Counter {
 
-    @InjectNumber(3)
-    private lateinit var times: Number
+    @InjectNumber(3) // need to add dependency "org.jetbrains.kotlin:kotlin-reflect"
+//    @field:InjectNumber(3) // if use java tools (bean.javaClass.declaredFields)
+    private var times: Number = 0
+//    private lateinit var times: Number // won't work in initializer block
 
-//    init {
-//        println("Initializer block that prints start times: $times")
-//        println("Initializer block that prints start value: $start")
-//    }
+    init {
+//        println("Phase 2")
+        println("Initializer block that prints start times: $times") // 0
+        println("Initializer block that prints start value: $start") // 1
+    }
 
     override fun count() {
-        println(start)
-        println(times)
+        println("After bean created and setup times value: $times") // 3
+        println("After bean created and PostConstruct start value: $start") // 10
+    }
+
+    @PostConstruct
+    fun updateStart() {
+        start = 10
     }
 }
--- spring-training/src/main/kotlin/annotations_processing/InjectNumberAnnotationBeanPostProcessor.kt
+++ spring-training/src/main/kotlin/annotations_processing/InjectNumberAnnotationBeanPostProcessor.kt
@@ -2,6 +2,7 @@ package annotations_processing
 
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
+import org.springframework.util.ReflectionUtils
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
@@ -12,7 +13,9 @@ class InjectNumberAnnotationBeanPostProcessor : BeanPostProcessor {
     //https://stackoverflow.com/questions/57582135/kotlin-annotation-not-in-fielddecleration-or-compiled-java
     //https://stackoverflow.com/questions/28391889/kotlin-reflection-get-list-of-fields
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
+        //        return super.postProcessBeforeInitialization(bean, beanName)
         val fields = bean::class.declaredMemberProperties
+        if (bean !is Counter) return bean
         fields.filterIsInstance<KMutableProperty<*>>().forEach { field ->
             val injectNumber = field.findAnnotation<InjectNumber>()
             println("${bean.javaClass} has declaredFields: $fields")
@@ -23,4 +26,17 @@ class InjectNumberAnnotationBeanPostProcessor : BeanPostProcessor {
         }
         return bean
     }
+
+//    if (bean::class.java == CounterImpl::class.java) {
+//        val fields = bean.javaClass.declaredFields
+//        fields.forEach {
+//            val injectNumber = it.getAnnotation(InjectNumber::class.java)
+//            if (injectNumber != null) {
+//                println("${bean.javaClass} has declaredFields: $fields")
+//                it.isAccessible = true
+//                ReflectionUtils.setField(it, bean, injectNumber.num)
+//            }
+//        }
+//    }
+//    return bean
 }
